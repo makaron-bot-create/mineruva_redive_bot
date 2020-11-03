1be892ac658177b45b47018a642fe0ee648a0308
makaron-bot-create <65615291+makaron-bot-create@users.noreply.github.com>
2020-11-03 15:30:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -461,7 +461,8 @@ async def loop():
     await client.wait_until_ready()
 
     guild = client.get_guild(599780162309062706)
-    channel = client.get_channel(741851480868519966)
+    server_rule_channel = guild.get_channel(741851480868519966)
+    clan_battle_channel = guild.get_channel(741851480868519966)
     role = guild.get_role(715250107058094100)  # 未3凸ロール
     clan_member_role = guild.get_role(687433139345555456)  # クラメンロール
 
@@ -478,9 +479,34 @@ async def loop():
         for member in clan_member:
             await member.add_roles(role)
 
-        await channel.send("クランメンバーに「未3凸」ロールを付与しました。")
+        await clan_battle_channel.send("クランメンバーに「未3凸」ロールを付与しました。")
+
+    # サーバー案内不要メッセージの削除
+    async for message in server_rule_channel.history():
+        if message.id != 749520003203596339:
+            await message.delete()
+    
 
 loop.start()
+
+
+# リアクション操作
+@client.event
+async def on_raw_reaction_add(payload):
+    guild = client.get_guild(payload.guild_id)
+    channel = guild.get_channel(payload.channel_id)
+    general_member_role = guild.get_role(687433546775789770)  # 一般メンバーロール
+
+    # サーバー案内
+    if channel.id == 749511208104755241:
+        if payload.emoji.name == \U00002705:
+
+            await channel.send(f"""
+{payload.member.mention} さん　こんにちわ。
+黒猫魔法学院への加入ありがとうございます。
+リアクションの確認が取れましたので、各種機能の制限を解除いたします。""")
+
+            await payload.member.add_roles(general_member_role)
 
 
 @client.event
