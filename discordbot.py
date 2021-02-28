f84583a35446625a4c22e763dad208841de2c088
makaron-bot-create <65615291+makaron-bot-create@users.noreply.github.com>
2021-03-01 00:08:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1899,7 +1899,7 @@ async def point_total(message):
             try:
                 if all([
                     member.id == mission_message.mentions[0].id,
-                    f"{now.year}年{now.month}月" in message_embed.fields[2].value
+                    f"{now.year}年2月" in message_embed.fields[2].value
                 ]):
                     get_point = re.search("(?<=\")[0-9]+(?= )", message_embed.fields[0].value).group()
                     points += int(get_point)
